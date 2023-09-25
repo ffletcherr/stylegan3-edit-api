4c19de764ff89ed2a8cfc368113370ffb5639492
ffletcherr <homayouneemoradi@gmail.com>
2023-09-25 18:54:19 +0330

    implement FaceEditorWrapper

--- face_editor.py
+++ face_editor.py
@@ -1,7 +1,4 @@
-import dataclasses
 import os
-import pprint
-import time
 from pathlib import Path
 
 import numpy as np
@@ -69,88 +66,74 @@ def compute_transforms(aligned_path, cropped_path):
         return inverse_transform
 
 
-model_path = "./pretrained_models/restyle_pSp_ffhq.pt"
-image_path = Path("./images/face_image.jpg")
-transform = transforms.Compose(
-    [
-        transforms.Resize((256, 256)),
-        transforms.ToTensor(),
-        transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5]),
-    ]
-)
+class FaceEditorWrapper:
+    def __init__(
+        self,
+        n_iters_per_batch=3,
+        resize_outputs=False,
+        model_path="./pretrained_models/restyle_pSp_ffhq.pt",
+    ) -> None:
+        self.net, self.opts = load_encoder(checkpoint_path=model_path)
+        self.opts.n_iters_per_batch = n_iters_per_batch
+        self.opts.resize_outputs = resize_outputs
+        self.avg_image = get_average_image(self.net)
+        self.editor = FaceEditor(
+            stylegan_generator=self.net.decoder, generator_type=GeneratorType.ALIGNED
+        )
+        self.transform = transforms.Compose(
+            [
+                transforms.Resize((256, 256)),
+                transforms.ToTensor(),
+                transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5]),
+            ]
+        )
 
-net, opts = load_encoder(checkpoint_path=model_path)
-pprint.pprint(dataclasses.asdict(opts))
+    def __call__(self, original_image, edit_direction="age", min_value=-5, max_value=5):
+        original_image = original_image.convert("RGB").resize((256, 256))
+        image_path = Path("./images/face_image.jpg")
+        original_image.save(image_path)
+        input_image = run_alignment(image_path)
+        cropped_image = crop_image(image_path)
+        joined = np.concatenate(
+            [input_image.resize((256, 256)), cropped_image.resize((256, 256))], axis=1
+        )
+        Image.fromarray(joined)
 
-original_image = Image.open(image_path).convert("RGB")
-original_image = original_image.resize((256, 256))
+        images_dir = Path("./images")
+        images_dir.mkdir(exist_ok=True, parents=True)
+        cropped_path = images_dir / f"cropped_{image_path.name}"
+        aligned_path = images_dir / f"aligned_{image_path.name}"
+        cropped_image.save(cropped_path)
+        input_image.save(aligned_path)
+        landmarks_transform = compute_transforms(
+            aligned_path=aligned_path, cropped_path=cropped_path
+        )
+        transformed_image = self.transform(input_image)
 
-input_image = run_alignment(image_path)
-cropped_image = crop_image(image_path)
-joined = np.concatenate(
-    [input_image.resize((256, 256)), cropped_image.resize((256, 256))], axis=1
-)
-Image.fromarray(joined)
+        with torch.no_grad():
+            result_batch, result_latents = run_on_batch(
+                inputs=transformed_image.unsqueeze(0).cuda().float(),
+                net=self.net,
+                opts=self.opts,
+                avg_image=self.avg_image,
+                landmarks_transform=torch.from_numpy(landmarks_transform)
+                .cuda()
+                .float(),
+            )
 
-images_dir = Path("./images")
-images_dir.mkdir(exist_ok=True, parents=True)
-cropped_path = images_dir / f"cropped_{image_path.name}"
-aligned_path = images_dir / f"aligned_{image_path.name}"
-cropped_image.save(cropped_path)
-input_image.save(aligned_path)
-landmarks_transform = compute_transforms(
-    aligned_path=aligned_path, cropped_path=cropped_path
-)
-
-n_iters_per_batch = 3
-opts.n_iters_per_batch = n_iters_per_batch
-opts.resize_outputs = False
-transformed_image = transform(input_image)
-
-avg_image = get_average_image(net)
-
-with torch.no_grad():
-    tic = time.time()
-    result_batch, result_latents = run_on_batch(
-        inputs=transformed_image.unsqueeze(0).cuda().float(),
-        net=net,
-        opts=opts,
-        avg_image=avg_image,
-        landmarks_transform=torch.from_numpy(landmarks_transform).cuda().float(),
-    )
-    toc = time.time()
-    print("Inference took {:.4f} seconds.".format(toc - tic))
-
-editor = FaceEditor(
-    stylegan_generator=net.decoder, generator_type=GeneratorType.ALIGNED
-)
-
-edit_direction = "age"
-min_value = -5
-max_value = 5
-
-print(f"Performing edit for {edit_direction}...")
-input_latent = torch.from_numpy(result_latents[0][-1]).unsqueeze(0).cuda()
-edit_images, edit_latents = editor.edit(
-    latents=input_latent,
-    direction=edit_direction,
-    factor_range=(min_value, max_value),
-    user_transforms=landmarks_transform,
-    apply_user_transformations=True,
-)
-print("Done!")
+        input_latent = torch.from_numpy(result_latents[0][-1]).unsqueeze(0).cuda()
+        edit_images, edit_latents = self.editor.edit(
+            latents=input_latent,
+            direction=edit_direction,
+            factor_range=(min_value, max_value),
+            user_transforms=landmarks_transform,
+            apply_user_transformations=True,
+        )
+        return edit_images
 
 
-def prepare_edited_result(edit_images):
-    if type(edit_images[0]) == list:
-        edit_images = [image[0] for image in edit_images]
-    res = np.array(edit_images[0].resize((512, 512)))
-    for image in edit_images[1:]:
-        res = np.concatenate([res, image.resize((512, 512))], axis=1)
-    res = Image.fromarray(res).convert("RGB")
-    return res
-
-
-res = prepare_edited_result(edit_images)
-
-res.save(f"edited_{image_path.name}")
+if __name__ == "__main__":
+    image_path = Path("./images/face_image.jpg")
+    original_image = Image.open(image_path)
+    face_editor = FaceEditorWrapper()
+    edit_images = face_editor(original_image)
