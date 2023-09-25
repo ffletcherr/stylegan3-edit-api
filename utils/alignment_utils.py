92d7d189007f93749a9d995bc08713bd9ac3ea94
ffletcherr <homayouneemoradi@gmail.com>
2023-09-25 23:48:09 +0330

    auto format

--- utils/alignment_utils.py
+++ utils/alignment_utils.py
@@ -29,8 +29,8 @@ def get_landmark(img, detector, predictor):
 
 
 def get_eyes_coors(landmark):
-    lm_eye_left = landmark[36: 42]  # left-clockwise
-    lm_eye_right = landmark[42: 48]  # left-clockwise
+    lm_eye_left = landmark[36:42]  # left-clockwise
+    lm_eye_right = landmark[42:48]  # left-clockwise
 
     # Calculate auxiliary vectors.
     eye_left = np.mean(lm_eye_left, axis=0)
@@ -39,7 +39,9 @@ def get_eyes_coors(landmark):
     return eye_left, eye_right
 
 
-def get_rotation_from_eyes(left_eye_unaligned, right_eye_unaligned, left_eye_aligned, right_eye_aligned):
+def get_rotation_from_eyes(
+    left_eye_unaligned, right_eye_unaligned, left_eye_aligned, right_eye_aligned
+):
     eye_to_eye1 = right_eye_unaligned - left_eye_unaligned
     eye_to_eye_normalized1 = eye_to_eye1 / np.linalg.norm(eye_to_eye1)
     eye_to_eye2 = right_eye_aligned - left_eye_aligned
@@ -53,10 +55,12 @@ def get_rotation_from_eyes(left_eye_unaligned, right_eye_unaligned, left_eye_ali
     return r
 
 
-def get_alignment_positions(dlib_image, detector, predictor, eyes_distance_only: bool = True):
+def get_alignment_positions(
+    dlib_image, detector, predictor, eyes_distance_only: bool = True
+):
     lm = get_landmark(dlib_image, detector, predictor)
 
-    lm_mouth_outer = lm[48: 60]  # left-clockwise
+    lm_mouth_outer = lm[48:60]  # left-clockwise
 
     # Calculate auxiliary vectors.
     eye_left, eye_right = get_eyes_coors(lm)
@@ -90,55 +94,93 @@ def get_fixed_cropping_transformation(c, x):
     d = np.hypot(x[0], x[1])
     d_hor = np.array([d, 0])
     d_ver = np.array([0, d])
-    quad = np.stack([c - d_hor - d_ver, c - d_hor + d_ver, c + d_hor + d_ver, c + d_hor - d_ver])
+    quad = np.stack(
+        [c - d_hor - d_ver, c - d_hor + d_ver, c + d_hor + d_ver, c + d_hor - d_ver]
+    )
     qsize = np.hypot(*x) * 2
 
     return quad, qsize
 
 
-def crop_face_by_transform(dlib_image, quad: np.ndarray, qsize: int, output_size: int = 1024,
-                           transform_size: int = 1024, enable_padding: bool = True):
+def crop_face_by_transform(
+    dlib_image,
+    quad: np.ndarray,
+    qsize: int,
+    output_size: int = 1024,
+    transform_size: int = 1024,
+    enable_padding: bool = True,
+):
     # convert dlib image to pillow image
     img = Image.fromarray(dlib_image)
 
     # Shrink.
     shrink = int(np.floor(qsize / output_size * 0.5))
     if shrink > 1:
-        rsize = (int(np.rint(float(img.size[0]) / shrink)), int(np.rint(float(img.size[1]) / shrink)))
+        rsize = (
+            int(np.rint(float(img.size[0]) / shrink)),
+            int(np.rint(float(img.size[1]) / shrink)),
+        )
         img = img.resize(rsize, Image.ANTIALIAS)
         quad /= shrink
         qsize /= shrink
 
     # Crop.
     border = max(int(np.rint(qsize * 0.1)), 3)
-    crop = (int(np.floor(min(quad[:, 0]))), int(np.floor(min(quad[:, 1]))), int(np.ceil(max(quad[:, 0]))),
-            int(np.ceil(max(quad[:, 1]))))
-    crop = (max(crop[0] - border, 0), max(crop[1] - border, 0), min(crop[2] + border, img.size[0]),
-            min(crop[3] + border, img.size[1]))
+    crop = (
+        int(np.floor(min(quad[:, 0]))),
+        int(np.floor(min(quad[:, 1]))),
+        int(np.ceil(max(quad[:, 0]))),
+        int(np.ceil(max(quad[:, 1]))),
+    )
+    crop = (
+        max(crop[0] - border, 0),
+        max(crop[1] - border, 0),
+        min(crop[2] + border, img.size[0]),
+        min(crop[3] + border, img.size[1]),
+    )
     if crop[2] - crop[0] < img.size[0] or crop[3] - crop[1] < img.size[1]:
         img = img.crop(crop)
         quad -= crop[0:2]
 
     # Pad.
-    pad = (int(np.floor(min(quad[:, 0]))), int(np.floor(min(quad[:, 1]))), int(np.ceil(max(quad[:, 0]))),
-           int(np.ceil(max(quad[:, 1]))))
-    pad = (max(-pad[0] + border, 0), max(-pad[1] + border, 0), max(pad[2] - img.size[0] + border, 0),
-           max(pad[3] - img.size[1] + border, 0))
+    pad = (
+        int(np.floor(min(quad[:, 0]))),
+        int(np.floor(min(quad[:, 1]))),
+        int(np.ceil(max(quad[:, 0]))),
+        int(np.ceil(max(quad[:, 1]))),
+    )
+    pad = (
+        max(-pad[0] + border, 0),
+        max(-pad[1] + border, 0),
+        max(pad[2] - img.size[0] + border, 0),
+        max(pad[3] - img.size[1] + border, 0),
+    )
     if enable_padding and max(pad) > border - 4:
         pad = np.maximum(pad, int(np.rint(qsize * 0.3)))
-        img = np.pad(np.float32(img), ((pad[1], pad[3]), (pad[0], pad[2]), (0, 0)), 'reflect')
+        img = np.pad(
+            np.float32(img), ((pad[1], pad[3]), (pad[0], pad[2]), (0, 0)), "reflect"
+        )
         h, w, _ = img.shape
         y, x, _ = np.ogrid[:h, :w, :1]
-        mask = np.maximum(1.0 - np.minimum(np.float32(x) / pad[0], np.float32(w - 1 - x) / pad[2]),
-                          1.0 - np.minimum(np.float32(y) / pad[1], np.float32(h - 1 - y) / pad[3]))
+        mask = np.maximum(
+            1.0 - np.minimum(np.float32(x) / pad[0], np.float32(w - 1 - x) / pad[2]),
+            1.0 - np.minimum(np.float32(y) / pad[1], np.float32(h - 1 - y) / pad[3]),
+        )
         blur = qsize * 0.02
-        img += (scipy.ndimage.gaussian_filter(img, [blur, blur, 0]) - img) * np.clip(mask * 3.0 + 1.0, 0.0, 1.0)
+        img += (scipy.ndimage.gaussian_filter(img, [blur, blur, 0]) - img) * np.clip(
+            mask * 3.0 + 1.0, 0.0, 1.0
+        )
         img += (np.median(img, axis=(0, 1)) - img) * np.clip(mask, 0.0, 1.0)
-        img = Image.fromarray(np.uint8(np.clip(np.rint(img), 0, 255)), 'RGB')
+        img = Image.fromarray(np.uint8(np.clip(np.rint(img), 0, 255)), "RGB")
         quad += pad[:2]
 
     # Transform.
-    img = img.transform((transform_size, transform_size), Image.QUAD, (quad + 0.5).flatten(), Image.BILINEAR)
+    img = img.transform(
+        (transform_size, transform_size),
+        Image.QUAD,
+        (quad + 0.5).flatten(),
+        Image.BILINEAR,
+    )
     if output_size < transform_size:
         img = img.resize((output_size, output_size), Image.ANTIALIAS)
 
@@ -156,38 +198,39 @@ def align_face(dlib_image, detector, predictor):
 def crop_face(dlib_image, detector, predictor, random_shift=0.0):
     c, x, y = get_alignment_positions(dlib_image, detector, predictor)
     if random_shift > 0:
-        c = (c + np.hypot(*x) * 2 * random_shift * np.random.normal(0, 1, c.shape))
+        c = c + np.hypot(*x) * 2 * random_shift * np.random.normal(0, 1, c.shape)
     quad, qsize = get_fixed_cropping_transformation(c, x)
     img = crop_face_by_transform(dlib_image, quad, qsize)
     return img
 
 
-def get_stylegan_transform(unaligned_dlib_image, aligned_dlib_image, detector, predictor):
-    try:
-        aligned_img = Image.fromarray(aligned_dlib_image)
-        aligned_img_lm = get_landmark(aligned_dlib_image, detector, predictor)
-        aligned_left_eye, aligned_right_eye = get_eyes_coors(aligned_img_lm)
-        unaligned_img_lm = get_landmark(unaligned_dlib_image, detector, predictor)
-        unaligned_left_eye, unaligned_right_eye = get_eyes_coors(unaligned_img_lm)
+def get_stylegan_transform(
+    unaligned_dlib_image, aligned_dlib_image, detector, predictor
+):
+    aligned_img = Image.fromarray(aligned_dlib_image)
+    aligned_img_lm = get_landmark(aligned_dlib_image, detector, predictor)
+    aligned_left_eye, aligned_right_eye = get_eyes_coors(aligned_img_lm)
+    unaligned_img_lm = get_landmark(unaligned_dlib_image, detector, predictor)
+    unaligned_left_eye, unaligned_right_eye = get_eyes_coors(unaligned_img_lm)
 
-        rotation_angle = get_rotation_from_eyes(left_eye_unaligned=unaligned_left_eye,
-                                                right_eye_unaligned=unaligned_right_eye,
-                                                left_eye_aligned=aligned_left_eye,
-                                                right_eye_aligned=aligned_right_eye)
+    rotation_angle = get_rotation_from_eyes(
+        left_eye_unaligned=unaligned_left_eye,
+        right_eye_unaligned=unaligned_right_eye,
+        left_eye_aligned=aligned_left_eye,
+        right_eye_aligned=aligned_right_eye,
+    )
 
-        rotated_aligned_dlib_image = aligned_img.rotate(rotation_angle)
-        rotated_aligned_image = Image.fromarray(rotated_aligned_dlib_image)
+    rotated_aligned_dlib_image = aligned_img.rotate(rotation_angle)
+    rotated_aligned_image = Image.fromarray(rotated_aligned_dlib_image)
 
-        rotated_aligned_img_lm = get_landmark(rotated_aligned_image, detector, predictor)
-        rotated_aligned_left_eye, rotated_aligned_right_eye = get_eyes_coors(rotated_aligned_img_lm)
+    rotated_aligned_img_lm = get_landmark(rotated_aligned_image, detector, predictor)
+    rotated_aligned_left_eye, rotated_aligned_right_eye = get_eyes_coors(
+        rotated_aligned_img_lm
+    )
 
-        translation = (unaligned_left_eye - rotated_aligned_left_eye) / aligned_img.size[0]
+    translation = (unaligned_left_eye - rotated_aligned_left_eye) / aligned_img.size[0]
 
-        transform = make_transform(translation, rotation_angle)
-        inverse_transform = np.linalg.inv(transform)
+    transform = make_transform(translation, rotation_angle)
+    inverse_transform = np.linalg.inv(transform)
 
-        return rotation_angle, translation, transform, inverse_transform
-
-    except Exception as e:
-        print(f"Failed aligning image. Got exception: {e}")
-        return None
+    return rotation_angle, translation, transform, inverse_transform
